b58adcdb4efaf3f9b887a982a5b388170fd97813
hendisantika <hendisantika@yahoo.co.id>
2025-01-08 18:20:06 +0700

    Add Start 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_crud/App.java
+++ src/main/java/id/my/hendisantika/vertx_crud/App.java
@@ -1,12 +1,51 @@
 package id.my.hendisantika.vertx_crud;
 
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
 
-public class App {
+public class App extends AbstractVerticle {
 
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new MyVerticle());
+  private JDBCClient client;
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    // Configure MySQL connection
+    client = JDBCClient.create(vertx, new JsonObject()
+      .put("url", "jdbc:mysql://localhost:3306/testdb")
+      .put("driver_class", "com.mysql.cj.jdbc.Driver")
+      .put("user", "root")
+      .put("password", "root")
+      .put("max_pool_size", 30));
+
+    // Create a router object
+    Router router = Router.router(vertx);
+
+    // Enable parsing of request bodies
+    router.route().handler(BodyHandler.create());
+
+    // Define GET endpoint
+    router.get("/users").handler(this::handleGetResource);
+    router.get("/users/:username").handler(this::handleGetByIdResource);
+    // Define POST endpoint
+    router.post("/users").handler(this::handlePostResource);
+
+    router.delete("/users/:username").handler(this::handleDeleteByIdResource);
+    router.put("/users/:username").handler(this::handleUpdateResource);
+
+
+    // Start the HTTP server
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8080, ar -> {
+        if (ar.succeeded()) {
+          System.out.println("Server started on port 8080");
+          startPromise.complete();
+        } else {
+          System.out.println("Server start failed: " + ar.cause());
+          startPromise.fail(ar.cause());
+        }
+      });
   }
-
 }
